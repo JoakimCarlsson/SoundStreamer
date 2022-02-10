3cafe650b4c4a8503bd645ee0b841800d50cb9ef
Joakim Carlsson <joakimcarlsson1994@gmail.com>
2022-02-10 08:19:35 +0100

    Add project files.

---
+++ src/app/SoundStreamer.Client/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SoundStreamer.Client
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
