426a6609ff86d432476aab4758b4279fd0071f4b
Joakim Carlsson <joakimcarlsson1994@gmail.com>
2022-02-10 08:32:07 +0100

    Fixed the inital boilerpalte code as a reference too futher pages that will be added.

---
+++ src/app/SoundStreamer.Client/Pages/Counter.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace SoundStreamer.Client.Pages;
+
+public partial class Counter
+{
+	private int _currentCount = 0;
+
+    private void IncrementCount()
+    {
+        _currentCount++;
+    }
+}
---
+++ src/app/SoundStreamer.Client/Pages/FetchData.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using SoundStreamer.Client.Data;
+
+namespace SoundStreamer.Client.Pages;
+
+public partial class FetchData
+{
+	private WeatherForecast[] _forecasts;
+    [Inject] private WeatherForecastService WeatherForecastService { get; set; }
+    protected override async Task OnInitializedAsync()
+    {
+        _forecasts = await WeatherForecastService.GetForecastAsync(DateTime.Now);
+    }
+}
