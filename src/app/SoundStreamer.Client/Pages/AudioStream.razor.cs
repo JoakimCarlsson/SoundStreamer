5d6774a410bf1ce7dc2b347ee35d8696549aa890
Joakim Carlsson <joakimcarlsson1994@gmail.com>
2022-02-14 15:37:36 +0100

    very basic implementation of a socket client so we can connect to our TcpServer

--- src/app/SoundStreamer.Client/Pages/AudioStream.razor.cs
+++ src/app/SoundStreamer.Client/Pages/AudioStream.razor.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
 using Microsoft.AspNetCore.Components;
 using SoundStreamer.Services;
 
@@ -8,16 +10,28 @@ public partial class AudioStream
 {
     [Inject] IAudioRecorder AudioRecorder { get; set; }
     [Inject] IAudioPlayer AudioPlayer { get; set; }
+    
     private async Task StartStream()
     {
-        if (AudioRecorder.IsRecording is false)
-        {
-            var audioQueue = await AudioRecorder.StartRecordingAsync();
-            await AudioPlayer.PlayAudioAsync(audioQueue);
-        }
-        else
-        {
-            AudioRecorder.StopRecording();
-        }
+        //fix hardcoded values
+        var ipHostInfo = await Dns.GetHostEntryAsync(Dns.GetHostName());
+        var ipAddress = IPAddress.Parse("192.168.0.107");
+        var remoteEndPoint = new IPEndPoint(ipAddress, 69);
+        var serverClient = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        
+        await serverClient.ConnectAsync(remoteEndPoint);
+
+
+        // if (AudioRecorder.IsRecording is false)
+        // {
+        //     var audioQueue = await AudioRecorder.StartRecordingAsync();
+        //     await AudioPlayer.PlayAudioAsync(audioQueue);
+        // }
+        // else
+        // {
+        //     AudioRecorder.StopRecording();
+        // }
+        
+        
     }
 }
